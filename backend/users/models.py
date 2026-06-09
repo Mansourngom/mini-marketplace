9b5353e6b966e098101c32a9d8d84fa7ef35e9e2
Mansourngom <mansourngom657@gmail.com>
2026-06-09 11:53:40 +0000

    feat/auth : systeme inscription et connexion JWT

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('vendeur', 'Vendeur'),
+        ('acheteur', 'Acheteur'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='acheteur')
+    telephone = models.CharField(max_length=20, blank=True)
+    ville = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.username
