ee9adf47e7d473d02189eb460743bef4b56d1748
DESKTOP-5UL4J7C\BMD Tech <amadou527@gmail.com>
2026-06-11 12:24:34 +0200

    feat/annonces : modeles et API annonces

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('annonces.urls')),
-]
+
+    # Auth JWT
+    path('api/auth/token/',         TokenObtainPairView.as_view(),  name='token_obtain'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(),     name='token_refresh'),
+
+    # Annonces
+    path('api/annonces/', include('annonces.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
